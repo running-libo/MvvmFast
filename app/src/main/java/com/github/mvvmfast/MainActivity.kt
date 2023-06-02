edf4e46121f9d419458c51aef7ea1b7610bbb238
libo3 <libo3@apusapps.com>
2023-06-02 20:10:13 +0800

    上传maven仓库，publish配置完成，上传maven仓库成功

--- app/src/main/java/com/github/mvvmfast/MainActivity.kt
+++ app/src/main/java/com/github/mvvmfast/MainActivity.kt
@@ -2,10 +2,14 @@ package com.github.mvvmfast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_flavor).apply {
+        }
     }
 }
